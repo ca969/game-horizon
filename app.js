0a4d5a49ccd5ba3442d0e18a6200a74cb3d99485
ca969 <ceyhunatagec@gmail.com>
2019-10-09 15:30:54 +0300

    config

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(function(err, req, res, next) {
 });
 
 app.get('*', function(req, res){
-  res.sendFile(path.join(__dirname + 'public/404.html'));
+  return res.send(path.join(__dirname + 'public/404.html'));
 });
 
 
